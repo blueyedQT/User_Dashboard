5b34c2f7ea7cceeeddc4ff9b39a9fadf0ca6c3f1
Katrina Sanford <blueyed_qt@hotmail.com>
2015-03-15 16:09:13 -0700

    Added bootstrap styling to forms

--- application/views/edit_profile.php
+++ application/views/edit_profile.php
@@ -1,33 +1,45 @@
 	<div class="container">
 		<h1>Edit Profile</h1>
 		<div class="row">
-			<form class="col-md-3" action="/edit_user" method="post">
+			<form role="form" class="col-md-4" action="/edit_user" method="post">
 				<legend>Edit Information</legend>
-				<p>Email Address:</p>
-				<input type="text" name="email" value="<?php echo $user_info['email'] ?>">
-				<p>First Name:</p>
-				<input type="text" name="first_name" value="<?php echo $user_info['first_name'] ?>">
-				<p>Last Name:</p>
-				<input type="text" name="last_name" value="<?php echo $user_info['last_name'] ?>">
+				<div class="form-group">
+					<label for="email">Email Address:</label>
+					<input class="form-control" type="text" name="email" value="<?php echo $user_info['email'] ?>">
+				</div>
+				<div class="form-group">
+					<label for="first_name">First Name:</label>
+					<input class="form-control" type="text" name="first_name" value="<?php echo $user_info['first_name'] ?>">
+				</div>
+				<div class="form-group">
+					<label for="last_name">Last Name:</label>
+					<input class="form-control" type="text" name="last_name" value="<?php echo $user_info['last_name'] ?>">
+				</div>
 				<input type="hidden" name="id" value="<?php echo $user_info['id'] ?>">
-				<input class="btn btn-primary" type="submit" name="submit" value="Save">
+				<button class="btn btn-primary" type="submit" name="submit">Save</button>
 			</form>
-			<form class="col-md-3 col-md-offset-2" action="/edit_password" method="post">
+			<form role="form" class="col-md-4 col-md-offset-2" action="/edit_password" method="post">
 				<legend>Change Password</legend>
-				<p>Password:</p>
-				<input type="password" name="password">
-				<p>Confirm Password:</p>
-				<input type="password" name="password2">
+				<div class="form-group">
+					<label for="password">Password:</label>
+					<input class="form-control" type="password" name="password">
+				</div>
+				<div class="form-group">
+					<label for="password2">Confirm Password:</label>
+					<input class="form-control" type="password" name="password2">
+				</div>
 				<input type="hidden" name="id" value="<?php echo $user_info['id'] ?>">
-				<p><input class="btn btn-primary" type="submit" name="submit" value="Update Password"></p>
-			</form>
-			<form class="form" action="/edit_description" method="post">
-				<legend>Edit Description</legend>
-				<textarea name="description"></textarea>
-				<input type="hidden" name="id" value="<?php echo $user_info['id'] ?>">
-				<p><input class="btn btn-primary" type="submit" name="submit" value="Save"></p>
+				<button class="btn btn-primary" type="submit" name="submit">Update Password</button>
 			</form>
 		</div>
+		<form role="form" action="/edit_description" method="post">
+			<legend>Edit Description</legend>
+			<div class="form-group">
+				<textarea class="form-control" name="description"></textarea>
+			</div>
+			<input type="hidden" name="id" value="<?php echo $user_info['id'] ?>">
+			<button class="btn btn-primary" type="submit" name="submit">Save<button>
+		</form>
 	</div>
 </body>
 </html>
--- application/views/edit_user.php
+++ application/views/edit_user.php
@@ -1,34 +1,46 @@
 	<div class="container">
 		<h1>Edit User #<?php echo $user_info['id'] ?></h1>
 		<div class="row">
-			<form class="col-md-3" action="/edit_user" method="post">
+			<form crole="form" class="col-md-4" action="/edit_user" method="post">
 				<legend>Edit Information</legend>
-				<p>Email Address:</p>
-				<input type="text" name="email" value="<?php echo $user_info['email'] ?>">
-				<p>First Name:</p>
-				<input type="text" name="first_name" value="<?php echo $user_info['first_name'] ?>">
-				<p>Last Name:</p>
-				<input type="text" name="last_name" value="<?php echo $user_info['last_name'] ?>">
-				<!-- format cleaner! -->
-				<p>User Level:</p><select name="user_level">
-<?php 			foreach($admin_levels as $level) { ?>
-					<option value="<?php echo $level['id']?>" 
-<?php 				if($level['id'] == $user_info['user_level']) {
-						echo 'selected="selected"'; 
-					} ?> ><?php echo $level['name'] ?></option>
-<?php 			} ?>
-				</select></p>
+				<div class="form-group">
+					<label for="email">Email Address:</label>
+					<input class="form-control" type="text" name="email" value="<?php echo $user_info['email'] ?>">
+				</div>
+				<div class="form-group">
+					<label for="first_name">First Name:</label>
+					<input class="form-control" type="text" name="first_name" value="<?php echo $user_info['first_name'] ?>">
+				</div>
+				<div class="form-group">
+					<label for="last_name">Last Name:</label>
+					<input class="form-control" type="text" name="last_name" value="<?php echo $user_info['last_name'] ?>">
+				</div>
+				<div class="form-group">
+					<label for="user_level">User Level:</label>
+					<select class="form-control" name="user_level">
+<?php 				foreach($admin_levels as $level) { ?>
+						<option value="<?php echo $level['id']?>" 
+<?php 					if($level['id'] == $user_info['user_level']) {
+							echo 'selected="selected"'; 
+						} ?> ><?php echo $level['name'] ?></option>
+<?php 				} ?>
+					</select></p>
+				</div>
 				<input type="hidden" name="id" value="<?php echo $user_info['id'] ?>">
-				<input class="btn btn-primary" type="submit" name="submit" value="Save">
+				<button class="btn btn-primary" type="submit" name="submit">Save</button>
 			</form>
-			<form class="col-md-3 col-md-offset-2" action="/edit_password" method="post">
+			<form role="form" class="col-md-4 col-md-offset-2" action="/edit_password" method="post">
 				<legend>Change Password</legend>
-				<p>Password:</p>
-				<input type="password" name="password">
-				<p>Confirm Password:</p>
-				<input type="password" name="password2">
+				<div class="form-group">
+					<label for="password">Password:</label>
+					<input class="form-control" type="password" name="password">
+				</div>
+				<div class="form-group">
+					<label for="password2">Confirm Password:</label>
+					<input class="form-control" type="password" name="password2">
+				</div>
 				<input type="hidden" name="id" value="<?php echo $user_info['id'] ?>">
-				<p><input class="btn btn-primary" type="submit" name="submit" value="Save"></p>
+				<button class="btn btn-primary" type="submit" name="submit">Update Password</button>
 			</form>
 		</div>
 	</div>
