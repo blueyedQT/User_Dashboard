734c7ba93744d0fa9f5652214696382d85c2c1e6
Katrina Sanford <blueyed_qt@hotmail.com>
2015-03-15 16:18:40 -0700

    Updated forms for add_user

--- application/views/add_user.php
+++ application/views/add_user.php
@@ -3,27 +3,34 @@
 			<h1 class="col-md-9">Add User</h1>
 			<a href="/dashboard/admin"><button class="btn btn-lg btn-primary">Return to Dashboard</button></a>
 		</div>
-		<form class="col-md-4" action="register_user" method="post">
-			<fieldset>
-<?php 			if(!empty($errors)) { ?>
-				<div class="alert alert-danger" role="alert">
-					<span class="sr-only">Error:</span>
-					<?php echo $errors ?>
-				</div>
-<?php			} ?>
-				<!-- Need to come up with a better way of getting them stacked inline than paragraph tags I think -->
-				<p><label>Email Address: </label></p>
-				<p><input type="text" name="email" placeholder="xyz@example.com"></p>
-				<p><label>First Name: </label></p>
-				<p><input type="text" name="first_name" placeholder="John"></p>
-				<p><label>Last Name: </label></p>
-				<p><input type="text" name="last_name" placeholder="Doe"></p>
-				<p><label>Password: </label></p>
-				<p><input type="password" name="password"></p>
-				<p><label>Password Confirmation: </label></p>
-				<p><input type="password" name="password2"></p>
-				<p><button type="submit" name="submit" class="btn">Add User!</button></p>
-			</fieldset>
+		<form role="form" class="col-md-5" action="register_user" method="post">
+<?php 		if(!empty($errors)) { ?>
+			<div class="alert alert-danger" role="alert">
+				<span class="sr-only">Error:</span>
+				<?php echo $errors ?>
+			</div>
+<?php		} ?>
+			<div class="form-group">
+				<label for="email">Email Address:</label>
+				<input class="form-control" type="text" name="email" placeholder="xyz@example.com">
+			</div>
+			<div class="form-group">
+				<label for="first_name">First Name:</label>
+				<input class="form-control" type="text" name="first_name" placeholder="John">
+			</div>
+			<div class="form-group">
+				<label for="last_name">Last Name:</label>
+				<input class="form-control" type="text" name="last_name" placeholder="Doe">
+			</div>
+			<div class="form-group">
+				<label for="password">Password:</label>
+				<input class="form-control" type="password" name="password">
+			</div>
+			<div class="form-group">
+				<label for="password2">Password Confirmation:</label>
+				<input class="form-control" type="password" name="password2">
+			</div>
+			<button type="submit" name="submit" class="btn btn-success">Add User</button>
 		</form>
 	</div>
 </body>
