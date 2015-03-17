405808ca2246c68a488295d07ac652b8a715c90f
Katrina Sanford <blueyed_qt@hotmail.com>
2015-03-17 12:31:37 -0700

    Fixed styling of profile page

--- application/views/profile.php
+++ application/views/profile.php
@@ -15,21 +15,16 @@
 				<div class="form-group">
 					<textarea class="form-control" rows="3" name="message"></textarea>
 				</div>
-				<button class="col-md-2 col-md-offset-10 btn btn-success" type="submit">Post Message</button>
+				<button class="col-md-2 col-md-offset-10 btn btn-primary" type="submit">Post Message</button>
 			</form>
 		</div>
 <?php	if(!empty($messages)) { 
 			foreach($messages as $message) { ?>
-<!-- 		<div class="message_container">
- -->
 		<div class="row">
-			<p class="col-md-10"><a href="#"><?php echo $message['message_name'] ?></a> wrote</p>
+			<h4 class="col-md-10"><a href="#"><?php echo $message['message_name'] ?></a> wrote</h4>
 			<p class="col-md-2 text-right"><?php echo $message['created_at'] ?></p>
 		</div>
-		<div class="row outline message"><?php echo $message['message'] ?></div>
-
-<!-- 				</div>	
- -->
+		<div class="row message outline"><?php echo $message['message'] ?></div>
 <?php 			if(!empty($comments)) {
 					foreach ($comments as $comment) {
 						if($comment['message_id'] == $message['id']) { ?>
@@ -38,17 +33,19 @@
 			<p class="col-md-2 text-right"><?php echo $comment['created_at'] ?></p>
 		</div>
 		<div class="row">
-			<div class="outline message col-md-11 col-md-offset-1"><?php echo $comment['comment'] ?></div>
+			<div class="outline comment col-md-11 col-md-offset-1"><?php echo $comment['comment'] ?></div>
 		</div>	
 <?php 					}
 					}
 				} ?>
 		<form role="form" class="col-md-offset-1" action="/post_comment/<?php echo $message['id'] ?>" method="post">
-			<div class="form-group">
+			<div class="row form-group">
 				<textarea class="form-control" name="comment">WRITE A COMMENT HERE</textarea>
 			</div>
 			<input type="hidden" name="user" value="<?php echo $user_info['id'] ?>">
-			<input class="col-md-2 col-md-offset-10 btn btn-success" type="submit" name="submit" value="post">
+			<div class="row">
+			<button class="col-md-2 col-md-offset-10 btn btn-success" type="submit">Post Comment</button>
+		</div>
 		</form>
 <?php	 	}
 		} ?>
