f3d990a39fbc81052c3a2795d85441ffe3f369c4
Katrina Sanford <blueyed_qt@hotmail.com>
2015-03-16 22:23:52 -0700

    Changed styling a bit

--- application/views/profile.php
+++ application/views/profile.php
@@ -1,24 +1,35 @@
 <?php var_dump($messages) ?>
 	<div class="container">
 		<div class="row">
-			<h1 class="col-md-4"><?php echo $user_info['first_name'].' '.$user_info['last_name'] ?></h1>
+			<h1><?php echo $user_info['first_name'].' '.$user_info['last_name'] ?></h1>
 		</div>
-		<p>Registered on: <?php echo $user_info['created'] ?></p>
-		<p>User ID: <?php echo $user_info['id'] ?></p>
-		<p>Email Address: <?php echo $user_info['email'] ?></p>
-		<p>Description: <?php echo $user_info['description'] ?></p>
-		<h1>Leave a message for <?php echo $user_info['first_name'] ?></h1>
-		<form role="form" action="/post_message/<?php echo $user_info['id'] ?>" method="post">
-			<textarea class="form-control" rows="3" name="message"></textarea>
-			<input class="col-md-2 col-md-offset-10 btn btn-success" type="submit" name="submit" value="Post">
-		</form>
+		<div class="row">
+			<p>Registered on: <?php echo $user_info['created'] ?></p>
+			<p>User ID: <?php echo $user_info['id'] ?></p>
+			<p>Email Address: <?php echo $user_info['email'] ?></p>
+			<p>Description: <?php echo $user_info['description'] ?></p>
+		</div>
+		<div class="row">
+			<h1>Leave a message for <?php echo $user_info['first_name'] ?></h1>
+			<form role="form" action="/post_message/<?php echo $user_info['id'] ?>" method="post">
+				<div class="form-group">
+					<textarea class="form-control" rows="3" name="message"></textarea>
+				</div>
+				<button class="col-md-2 col-md-offset-10 btn btn-success" type="submit">Post Message</button>
+			</form>
+		</div>
 <?php	if(!empty($messages)) { 
 			foreach($messages as $message) { ?>
+<!-- 		<div class="message_container">
+ -->
 		<div class="row">
 			<p class="col-md-10"><a href="#"><?php echo $message['message_name'] ?></a> wrote</p>
 			<p class="col-md-2 text-right"><?php echo $message['created_at'] ?></p>
 		</div>
-		<div class="outline message"><?php echo $message['message'] ?></div>
+		<div class="row outline message"><?php echo $message['message'] ?></div>
+
+<!-- 				</div>	
+ -->
 <?php 			if(!empty($comments)) {
 					foreach ($comments as $comment) {
 						if($comment['message_id'] == $message['id']) { ?>
@@ -28,12 +39,14 @@
 		</div>
 		<div class="row">
 			<div class="outline message col-md-11 col-md-offset-1"><?php echo $comment['comment'] ?></div>
-		</div>		
+		</div>	
 <?php 					}
 					}
 				} ?>
-		<form class="col-md-offset-1" action="/post_comment/<?php echo $message['id'] ?>" method="post">
-			<textarea class="form-control" name="comment">WRITE A COMMENT HERE</textarea>
+		<form role="form" class="col-md-offset-1" action="/post_comment/<?php echo $message['id'] ?>" method="post">
+			<div class="form-group">
+				<textarea class="form-control" name="comment">WRITE A COMMENT HERE</textarea>
+			</div>
 			<input type="hidden" name="user" value="<?php echo $user_info['id'] ?>">
 			<input class="col-md-2 col-md-offset-10 btn btn-success" type="submit" name="submit" value="post">
 		</form>
