b63047ecc7837fb6b7661e25b0fc5461ccf253e1
Katrina Sanford <blueyed_qt@hotmail.com>
2015-03-17 13:21:13 -0700

    Added function for timeAgo feature

--- application/views/profile.php
+++ application/views/profile.php
@@ -1,4 +1,17 @@
-	<div class="container">
+<?php  	function timeAgo($timestamp) {
+			$timeCalc = time() - strtotime($timestamp);
+			if ($timeCalc > (24 * 60 * 60)) {
+				$time = date('M d, Y', strtotime($timestamp));
+				return $time;
+			} else if ($timeCalc > (60 * 60)) {
+				$timeCalc = round($timeCalc/60/60) . " hours ago";
+			} else if ($timeCalc > 60) {
+				$timeCalc = round($timeCalc/60) . " minutes ago";
+			}
+			return $timeCalc;
+		} ?>
+
+		<div class="container">
 		<div class="row">
 			<h1><?php echo $user_info['first_name'].' '.$user_info['last_name'] ?></h1>
 		</div>
@@ -22,7 +35,7 @@
 				if($message['id'] !== null) { ?>
 		<div class="row">
 			<h4 class="col-md-10"><a href="/profile/<?php echo $message['user_id'] ?>"><?php echo $message['message_name'] ?></a> wrote</h4>
-			<p class="col-md-2 text-right"><?php echo $message['created_at'] ?></p>
+			<p class="col-md-2 text-right"><?php echo timeAgo($message['created_at']) ?></p>
 		</div>
 		<div class="row message outline"><?php echo $message['message'] ?></div>
 <?php 				if(!empty($comments)) {
@@ -30,7 +43,7 @@
 							if($comment['message_id'] == $message['id']) { ?>
 		<div class="row">
 			<p class="col-md-9 col-md-offset-1"><a href="#"><?php echo $comment['comment_name'] ?></a> wrote</p>
-			<p class="col-md-2 text-right"><?php echo $comment['created_at'] ?></p>
+			<p class="col-md-2 text-right"><?php echo timeAgo($comment['created_at']) ?></p>
 		</div>
 		<div class="row">
 			<div class="outline comment col-md-11 col-md-offset-1"><?php echo $comment['comment'] ?></div>
